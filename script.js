4e76ec1275a25832524460d242846a8ef2a55a67
Poliana Calegario <135210042+polianacalegario@users.noreply.github.com>
2023-08-21 20:17:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Cria uma instância do objeto XMLHttpRequest
+    const postContainer = document.getElementById('post-container');
+
+    // Cria uma nova instância do objeto XMLHttpRequest
     const xhr = new XMLHttpRequest();
 
-    // Define a função de tratamento do evento de carregamento
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            const posts = JSON.parse(xhr.responseText);
-            const postContainer = document.getElementById('post-container');
+    // Configura a requisição
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
 
-            posts.forEach(post => {
-                const postElement = document.createElement('div');
-                postElement.classList.add('post');
-                postElement.innerHTML = `
-                    <h2>${post.title}</h2>
-                    <p>${post.body}</p>
-                `;
-                postContainer.appendChild(postElement);
-            });
-        } else {
-            console.error('Erro ao obter os posts:', xhr.statusText);
+    // Define o callback para quando a requisição estiver pronta
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const posts = JSON.parse(xhr.responseText);
+
+                posts.forEach(post => {
+                    const postElement = document.createElement('div');
+                    postElement.classList.add('post');
+                    postElement.innerHTML = `
+                        <h2>${post.title}</h2>
+                        <p>${post.body}</p>
+                    `;
+                    postContainer.appendChild(postElement);
+                });
+            } else {
+                console.error('Erro ao obter os posts:', xhr.statusText);
+            }
         }
     };
 
-    // Define a função de tratamento do evento de erro
-    xhr.onerror = function() {
-        console.error('Erro de conexão');
-    };
-
-    // Configura a solicitação
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
-
-    // Envia a solicitação
+    // Envia a requisição
     xhr.send();
 });
