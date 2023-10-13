34bf4f2be75d90913cc40e88a6019bb014e5cbcc
Poliana Calegario <135210042+polianacalegario@users.noreply.github.com>
2023-10-13 14:19:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const postContainer = document.getElementById('post-container');
+    // Faz uma chamada assíncrona para obter os posts
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => {
+            const postContainer = document.getElementById('post-container');
 
-    // Cria uma nova instância do objeto XMLHttpRequest
-    const xhr = new XMLHttpRequest();
-
-    // Configura a requisição
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
-
-    // Define o callback para quando a requisição estiver pronta
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                const posts = JSON.parse(xhr.responseText);
-
-                posts.forEach(post => {
-                    const postElement = document.createElement('div');
-                    postElement.classList.add('post');
-                    postElement.innerHTML = `
-                        <h2>${post.title}</h2>
-                        <p>${post.body}</p>
-                    `;
-                    postContainer.appendChild(postElement);
-                });
-            } else {
-                console.error('Erro ao obter os posts:', xhr.statusText);
-            }
-        }
-    };
-
-    // Envia a requisição
-    xhr.send();
+            posts.forEach(post => {
+                const postElement = document.createElement('div');
+                postElement.classList.add('post');
+                postElement.innerHTML = `
+                    <h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                `;
+                postContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao obter os posts:', error);
+        });
 });
+
+
